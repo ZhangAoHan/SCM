840c2b792f039fb9d1dfd5b5f431e0f5333a9973
ZhangAoHan <1647110671@qq.com>
2020-12-24 16:15:57 +0800

    。

---
+++ Arduino工程/Arduino MPU6050串口输出角度（波特率9600 卡尔曼滤波）/Mpu6050_Kalman_for_Arduino/Mpu6050_Kalman_for_Arduino.ino
@@ -0,0 +1,17 @@
+int Left_0=22;int Left_1=23;int Left_2=24;int Left_3=25;
+int Left_4=26;int Left_5=27;int Left_6=28;int Left_7=29;
+int Left_8=30;int Left_9=31;int Left_10=32;
+int Right_0=42;int Right_1=43;int Right_2=44;int Right_3=45;
+int Right_4=46;int Right_5=47;int Right_6=48;int Right_7=49;
+int Right_8=50;int Right_9=21;int Right_10=52;
+void setup() {
+int i;
+for(i=42;i<53;i++);
+{ pinMode(i,OUTPUT); }
+for(i=22;i<33;i++);
+{ pinMode(i,OUTPUT); }
+}
+void loop() {
+  
+
+}
